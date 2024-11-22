791187cb392f9c31c7320cecf0fbfba2c0a2595c
Jeremy Cook <jscook55@gmail.com>
2024-11-22 09:30:59 -0500

    Update blink-GP0.py

--- RP2040-basic-m1/code/blink-GP0.py
+++ RP2040-basic-m1/code/blink-GP0.py
@@ -12,3 +12,6 @@ while True:
     time.sleep(.5)
     lightA.value = False
     time.sleep(.5)
+    
+# Note Board Pin lables are per chip numbers - 2, 3, 4, 5
+# Which correspond to GPIO numbers - 0, 1, 2, 3
